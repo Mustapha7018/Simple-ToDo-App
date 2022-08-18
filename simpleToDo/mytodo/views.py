e93a40c87616fc018801eae9609262d4befabd48
Mustapha Adams <adamsmustapha419@gmail.com>
2022-08-18 01:46:24 +0000

    views and home change

--- simpleToDo/mytodo/views.py
+++ simpleToDo/mytodo/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
 from django.utils import timezone
 from mytodo.models import Todo
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def home(request):
-    todo_items = Todo.objects.all().order_by("added_date")
-    return render(request, 'mytodo/home.html')
+    todo_items = Todo.objects.all().order_by("-added_date")
+    return render(request, 'mytodo/home.html', {'todo_items': todo_items})
 
 def add_todo(request):
     current_date = timezone.now()
     content = request.POST["content"]
     created_obj = Todo.objects.create(added_date=current_date, text=content)
-    return render(request, 'mytodo/home.html', {"todo_items":todo_items})
+    return HttpResponseRedirect("/")
+    
 
 
 
